561796e16cdd69b419569c673c7536377be5613e
averak <abe12@mccc.jp>
2021-05-10 12:33:58 +0900

    [fix] change sheet name

--- src/config/SheetConfig.ts
+++ src/config/SheetConfig.ts
@@ -4,7 +4,7 @@ export const SHEET_NAME = {
   personal_config: "個人設定",
   post_config: "投稿設定",
   slack_channel: "チャンネル一覧",
-  slack_event: "イベント一覧",
+  slack_event: "イベントログ",
   env: "環境変数",
   log: "ログ",
 };
